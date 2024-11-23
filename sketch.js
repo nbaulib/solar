4e9d538aec6f005aed7aaf1d59be3eb7e4eb8056
Michelle <michelle.tlee888@gmail.com>
2024-11-23 01:21:57 -0500

    rotating text

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let sounds = [];
+
 
 // credit: sam heckle - sound array demo 
 // https://editor.p5js.org/samheckle/sketches/wY2UFabY6D
@@ -9,11 +9,15 @@ let sounds = [];
 //   }
 // }
 
+let sounds = [];
 let currentPage = 0;
+let radius;
+let str = "birth   sun   spirit   freedom   ";
 
 function setup() {
   createCanvas(900, 600);
   textFont("Courier New");
+  radius = min(width,height)/3;
 }
 
 function draw() {
@@ -21,11 +25,13 @@ function draw() {
   // switch between intro page and music page 
 
   musicPage()
+  textCircle();
 
   // if (currentPage == 0) { // page 0 is intro page
   //   introPage(); // runs introPage
   // } else if (currentPage == 1) { // page 1 is music page
   //   musicPage(); // runs musicPage
+  //   textCircle();
   // }
 
 }
@@ -57,6 +63,45 @@ function musicPage() {
   background("#B1BC83");
   fill("black");
   circle(width/2, height, 900);
+}
+
+
+// text on circle tutorial: https://www.youtube.com/watch?v=pBtgOdAnoJI 
+function textCircle() {
+
+  // push();
+  // noFill();
+  // stroke(0,150,255);
+  // circle(width/2, height, radius*4); // circle as a guide
+  // pop();
+
+  let angleBtwLetters = TWO_PI/str.length; 
+  // calculates the angle btw letters in a str, based on the # of letters in the str
+
+  push();
+
+  translate(width/2, height);
+  textAlign(CENTER, BASELINE);
+  textSize(radius/5) // makes text size dynamic
+
+  for(let i = 0; i < str.length; i++) {
+
+    push();
+
+    let angle = i*angleBtwLetters + (frameCount * 0.0025); 
+    // adding frameCount to create rotation
+
+    rotate(angle);
+    translate(0, -radius * 1.89);  // moves the text along the circle
+
+    fill("white");
+    noStroke();
+    text(str[i], 0, 0);
+
+    pop();
+
+  }
+  pop();
 }
 
 function bass() {
